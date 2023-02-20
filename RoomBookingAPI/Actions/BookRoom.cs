99237b789d19b2b845b3151ed411eb2b1afd159a
James Lewis <james.lewis@llareggub.io>
2023-02-20 19:13:21 +0000

    book room: pass failing test to assert that a null argument exception is thrown if the request full name is empty

--- RoomBookingAPI/Actions/BookRoom.cs
+++ RoomBookingAPI/Actions/BookRoom.cs
@@ -4,6 +4,7 @@ public class BookRoom
 {
     public BookingConfirmation Process(BookingRequest request)
     {
+        if (string.IsNullOrEmpty(request.FullName)) throw new ArgumentNullException("FullName");
         return new BookingConfirmation(request);
     }
 }
