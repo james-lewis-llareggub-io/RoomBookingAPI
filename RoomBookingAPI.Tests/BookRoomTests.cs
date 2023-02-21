0391e258d2d7f5e58b5eb556afa8786052f3fea9
James Lewis <james.lewis@llareggub.io>
2023-02-21 08:15:04 +0000

    book room: add failing tests to assert that a null argument exception is thrown if the request email is empty or the date is DateTime.Min

--- RoomBookingAPI.Tests/BookRoomTests.cs
+++ RoomBookingAPI.Tests/BookRoomTests.cs
@@ -3,35 +3,57 @@ namespace RoomBookingAPI;
 public class BookRoomTests
 {
     private readonly BookRoom _bookRoom;
+    private readonly BookingRequest _bookingRequest;
 
     public BookRoomTests()
     {
         _bookRoom = new BookRoom();
-    }
-
-    [Fact]
-    public void Should_return_booking_confirmation_with_original_request()
-    {
-        var request = new BookingRequest
+        _bookingRequest = new BookingRequest
         {
             FullName = "Test Name",
             Email = "test@somewhere.com",
-            Date = new DateTime()
+            Date = DateTime.Now
         };
-
-        var confirmation = _bookRoom.Process(request);
+    }
+    
+    [Fact]
+    public void Should_return_booking_confirmation_with_original_request()
+    {
+        var confirmation = _bookRoom.Process(_bookingRequest);
         confirmation.BookingRequest.Should().NotBeNull();
-        request.FullName.Should().Be(confirmation.BookingRequest.FullName);
-        request.Email.Should().Be(confirmation.BookingRequest.Email);
-        request.Date.Should().Be(confirmation.BookingRequest.Date);
+        var actual = confirmation.BookingRequest;
+        actual.FullName.Should().Be(_bookingRequest.FullName);
+        actual.Email.Should().Be(_bookingRequest.Email);
+        actual.Date.Should().Be(_bookingRequest.Date);
     }
 
-    [Fact]
-    public void Should_throw_null_exception_if_request_fullname_is_empty()
+    public class NullRequestParameters : BookRoomTests
     {
-        var request = new BookingRequest();
-        var action = () => _bookRoom.Process(request);
-        action.Should().Throw<ArgumentNullException>()
-            .WithParameterName(nameof(request.FullName));
+        [Fact]
+        public void Should_throw_null_exception_if_request_fullname_is_empty()
+        {
+            var request = new BookingRequest();
+            var action = () => _bookRoom.Process(request);
+            action.Should().Throw<ArgumentNullException>()
+                .WithParameterName(nameof(request.FullName));
+        }
+
+        [Fact]
+        public void Should_throw_null_exception_if_request_email_is_empty()
+        {
+            _bookingRequest.Email = string.Empty;
+            var action = () => _bookRoom.Process(_bookingRequest);
+            action.Should().Throw<ArgumentNullException>()
+                .WithParameterName(nameof(_bookingRequest.Email));
+        }
+
+        [Fact]
+        public void Should_throw_null_exception_if_request_date_is_empty()
+        {
+            _bookingRequest.Date = DateTime.MinValue;
+            var action = () => _bookRoom.Process(_bookingRequest);
+            action.Should().Throw<ArgumentNullException>()
+                .WithParameterName(nameof(_bookingRequest.Date));
+        }
     }
 }
